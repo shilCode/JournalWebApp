3460e78b97aee68a0c6c7c393350acc4901240ba
shilcode <2850542@gmail.com>
2025-07-13 08:34:05 +0200

    cors error resolve

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,30 @@
-const UserRouter = require("./routes/user.js")
-const express = require("express")
+const UserRouter = require("./routes/user.js");
+const express = require("express");
 const mongo = require("mongoose");
 const PostRouter = require("./routes/post.js");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-    origin: [
-        "https://test-auto-notes-backend.vercel.app/",
-        "http://localhost:5173"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+  origin: [
+    "https://test-auto-notes-backend.vercel.app/",
+    "http://localhost:5173",
+    "https://journal-app-backend-phi.vercel.app/api/v1/user/signin",
+    "https://test-app-notes-five.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
-app.options('*', cors(corsOptions)); // Handle preflight for all routes
+app.options("*", cors(corsOptions)); // Handle preflight for all routes
 app.use(cors(corsOptions));
-app.use(express.json())
+app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server live at http://localhost:${port}`)
-})
+  console.log(`Server live at http://localhost:${port}`);
+});
 
-app.use('/api/v1/user', UserRouter)
-app.use('/api/v1/blog', PostRouter)
+app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/blog", PostRouter);
